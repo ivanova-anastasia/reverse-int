4be93400a0369f53165f817f657877031789d96c
Anastasia Ivanova <amastasia.ivanova@minsk.leveris.com>
2020-10-21 15:28:39 +0300

    feat: add reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return Number(
+        Array.from(String(Math.abs(n)), Number)
+            .reverse()
+            .join("")
+    );
+};
